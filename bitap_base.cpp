92879a3c60ceb89c50ab714cc448b7b9d1c0d4f5
Dheeraja7252 <dheeraja.rajreddygari@students.iiit.ac.in>
2022-05-07 09:30:24 +0530

    parallel bitmask

--- bitap_base.cpp
+++ bitap_base.cpp
@@ -1,6 +1,7 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "openmp-use-default-none"
 
+#include <iostream>
 #include "include.h"
 
 using namespace std;
@@ -11,12 +12,31 @@ vector<int> bitap_base(string &t, string &p) {
     R.set();
 
     vector<bitset<P_LEN + 1>> pattern_mask(alpha, R);
-    R[0] = false;
     //build pattern mask
-//#pragma omp parallel for
-// TODO: Parallise this by heving diff threads mask diff parts of the string
-    for (int i = 0; i < P_LEN; i++)
-        pattern_mask[p[i] - 'A'][i] = false;
+#pragma omp parallel for
+    for(int it = 0; it < 4; it++) {
+        vector<bitset<P_LEN + 1>> t_pattern_mask(alpha, R);
+
+        int begin = it * (P_LEN / 4);
+        int end = min((it + 1) * (P_LEN / 4), P_LEN);
+        if (it == 3)
+            end = P_LEN;
+        for (int i = begin; i < end; i++)
+            t_pattern_mask[p[i] - 'A'][i] = false;
+
+
+#pragma omp critical
+        {
+            for(int i = 0; i < alpha; i++) {
+                pattern_mask[i] &= t_pattern_mask[i];
+            }
+        };
+    };
+
+//    for (int i = 0; i < P_LEN; i++)
+//        pattern_mask[p[i] - 'A'][i] = false;
+
+    R[0] = false;
 
     for (int i = 0; i < T_LEN; i++) {
         R |= pattern_mask[t[i] - 'A'];
